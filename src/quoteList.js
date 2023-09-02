29961fb5836bc30c7fee682e77ad1b7f872cca0a
AmilLabib <72798992+AmilLabib@users.noreply.github.com>
2023-09-02 16:28:08 +0700

    Add files via upload

---
+++ src/quoteList.js
@@ -0,0 +1,132 @@
+export const quotesList = [
+  {
+    text: "If you can’t explain it simply, you don’t understand it well enough.",
+    author: "Albert Einstein",
+  },
+
+  {
+    text: "I believe every human has a finite number of heartbeats. I don’t intend to waste any of mine.",
+    author: "Neil Armstrong",
+  },
+
+  {
+    text: "I have not failed. I’ve just found 10,000 ways that won’t work",
+    author: "Thomas A. Edison",
+  },
+
+  {
+    text: "Many of life’s failures are people who did not realize how close they were to success when they gave up.",
+    author: "Thomas A. Edison",
+  },
+
+  {
+    text: "It is not the strongest of the species that survive, nor the most intelligent, but the one most responsive to change.",
+    author: "Charles Darwin",
+  },
+
+  {
+    text: "It does not matter how slowly you go, so long as you do not stop.",
+    author: "Confucius",
+  },
+
+  {
+    text: "All our dreams can come true if we have the courage to pursue them.",
+    author: "Walt Disney",
+  },
+
+  {
+    text: "No one can make you feel inferior without your consent.",
+    author: "Eleanor Roosevelt",
+  },
+
+  {
+    text: "Only put off until tomorrow what you are willing to die having left undone.",
+    author: "Pablo Picasso",
+  },
+
+  {
+    text: "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.",
+    author: "Mark Twain",
+  },
+
+  {
+    text: "Failure is another steppingstone to greatness.",
+    author: "Oprah Winfrey",
+  },
+
+  {
+    text: "The best and most beautiful things in the world cannot be seen or even touched – they must be felt with the heart",
+    author: "Helen Keller",
+  },
+
+  {
+    text: "When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one that has been opened for us.",
+    author: "Helen Keller",
+  },
+
+  {
+    text: "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+    author: "Mahatma Gandhi",
+  },
+
+  {
+    text: "To live is the rarest thing in the world. Most people exist, that is all.",
+    author: "Oscar Wilde",
+  },
+  {
+    text: "That it will never come again is what makes life so sweet.",
+    author: "Emily Dickinson",
+  },
+  {
+    text: "It is never too late to be what you might have been.",
+    author: "George Eliot",
+  },
+  {
+    text: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    author: "Ralph Waldo Emerson",
+  },
+  {
+    text: "Pain is inevitable. Suffering is optional.",
+    author: "Haruki Murakami",
+  },
+  {
+    text: "All the world's a stage, and all the men and women merely players.",
+    author: "William Shakespeare",
+  },
+  {
+    text: "Be kind, for everyone you meet is fighting a hard battle.",
+    author: "Plato",
+  },
+  {
+    text: "Unable are the loved to die for love is immortality.",
+    author: "Emily Dickinson",
+  },
+  {
+    text: "Let me live, love, and say it well in good sentences.",
+    author: "Sylvia Plath",
+  },
+  {
+    text: "Don't let your happiness depend on something you may lose.",
+    author: "C.S. Lewis",
+  },
+  {
+    text: "We are all broken, that's how the light gets in.",
+    author: "Ernest Hemingway",
+  },
+  {
+    text: "Appreciation is a wonderful thing. It makes what is excellent in others belong to us as well.",
+    author: "Voltaire",
+  },
+  {
+    text: "Life is tough my darling, but so are you.",
+    author: "Stephanie Bennett Henry",
+  },
+  {
+    text: "Self-awareness and self-love matter. Who we are is how we lead.",
+    author: "Brene Brown",
+  },
+  {
+    text: "Amateurs sit and wait for inspiration, the rest of us just get up and go to work.",
+    author: "Stephen King",
+  },
+];
